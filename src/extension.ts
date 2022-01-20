6b719b22cd15618e621a01b13f764e19a1f6ddc9
Digifox03 <digifox03@protonmail.com>
2022-01-20 23:10:01 +0100

    Implement completion provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,54 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {}
+import entities from './entities.json';
+import functions from './functions.json';
+
+const allEntities = entities.map((name) =>
+	new vscode.CompletionItem(
+		name, vscode.CompletionItemKind.Variable
+	)
+);
+
+const allFunctions = functions.map((name) =>
+	new vscode.CompletionItem(
+		name, vscode.CompletionItemKind.Function
+	)
+);
+
+const defaultCompletion = new vscode.CompletionItem(
+	"default", vscode.CompletionItemKind.Variable
+);
+
+class ParameterProvider implements vscode.CompletionItemProvider {
+	public provideCompletionItems(document: vscode.TextDocument) {
+		if (document.isUntitled) {
+			return [defaultCompletion, ...allEntities];
+		}
+		let entityName = document.fileName.replace("[.][^.]+$", "");	
+		if (entityName in entities) {
+			const completion = new vscode.CompletionItem(
+				entityName, vscode.CompletionItemKind.Variable
+			);
+			return [completion, defaultCompletion];
+		}
+		return allEntities;
+	}
+}
+
+class FunctionProvider implements vscode.CompletionItemProvider {
+	public provideCompletionItems() {
+		return allFunctions;
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	const parameterProvider = vscode.languages.registerCompletionItemProvider(
+		"reselect", new ParameterProvider()
+	);
+	const functionProvider = vscode.languages.registerCompletionItemProvider(
+		"reselect", new FunctionProvider(), "."
+	);
+	context.subscriptions.push(parameterProvider, functionProvider);
+}
+
 export function deactivate() {}
