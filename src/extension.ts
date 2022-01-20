0ac4c20bb418fefb7e2b102bd96ce12a85af61b8
Digifox03 <digifox03@protonmail.com>
2022-01-21 00:20:52 +0100

    Add syntax keywords to completion

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,19 @@ import * as vscode from 'vscode';
 import entities from './entities.json';
 import functions from './functions.json';
 
+const allSyntax = [
+	new vscode.CompletionItem("if", vscode.CompletionItemKind.Keyword),
+	new vscode.CompletionItem("then", vscode.CompletionItemKind.Keyword),
+	new vscode.CompletionItem("elseif", vscode.CompletionItemKind.Keyword),
+	new vscode.CompletionItem("else", vscode.CompletionItemKind.Keyword),
+	new vscode.CompletionItem("let", vscode.CompletionItemKind.Keyword),
+	new vscode.CompletionItem("in", vscode.CompletionItemKind.Keyword),
+	new vscode.CompletionItem("end", vscode.CompletionItemKind.Keyword),
+	new vscode.CompletionItem("and", vscode.CompletionItemKind.Operator),
+	new vscode.CompletionItem("or", vscode.CompletionItemKind.Operator),
+	new vscode.CompletionItem("not", vscode.CompletionItemKind.Operator),
+];
+
 const allEntities = entities.map((name) =>
 	new vscode.CompletionItem(
 		name, vscode.CompletionItemKind.Variable
@@ -18,6 +31,12 @@ const allFunctions = functions.map((name) =>
 const defaultCompletion = new vscode.CompletionItem(
 	"default", vscode.CompletionItemKind.Variable
 );
+
+class SyntaxProvider implements vscode.CompletionItemProvider {
+	public provideCompletionItems() {
+		return allSyntax;
+	}
+}
 
 class ParameterProvider implements vscode.CompletionItemProvider {
 	public provideCompletionItems(document: vscode.TextDocument) {
@@ -48,7 +67,14 @@ export function activate(context: vscode.ExtensionContext) {
 	const functionProvider = vscode.languages.registerCompletionItemProvider(
 		"reselect", new FunctionProvider(), "."
 	);
-	context.subscriptions.push(parameterProvider, functionProvider);
+	const syntaxProvider = vscode.languages.registerCompletionItemProvider(
+		"reselect", new SyntaxProvider()
+	);
+	context.subscriptions.push(
+		parameterProvider,
+		functionProvider,
+		syntaxProvider
+	);	
 }
 
 export function deactivate() {}
